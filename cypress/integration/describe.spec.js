21cfdbc869ac231d891bcabce3733a34a03e5666
flaviafreitas <flavia.moreira.freitas@gmail.com>
2020-06-21 16:08:19 -0300

    init project cypress

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,21 @@
+///<reference types="cypress" />
+
+it('A external test...', () => {
+
+})
+
+describe('Should group tests...', () => { 
+  describe('Shoud group more specifc tests...', ( ) => {
+    it("A specif test...", () => {
+    
+    })
+  })
+  describe('Shoud group more specifc tests 2...', ( ) => {
+    it("A specif test 2...", () => {
+    
+    })
+  })
+  it('A internal test...', () => {
+
+  })
+})
